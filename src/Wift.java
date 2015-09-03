cce16173b8ca93260916eb38133031a4bd0dcf71
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-09-03 10:29:59 +0200

    Add "BASIC functionality" comments :star:

--- src/Wift.java
+++ src/Wift.java
@@ -2,6 +2,13 @@ public class Wift {
   
   /** 
    *  Wift - The BASIC Programming Language
+   *  
+   *        BASIC FUNCTIONALITY:
+   *            - STRINGS []
+   *            - INTEGERS []
+   *            - ARITHMETIC []
+   *            - VARIABLES []
+   *  
    */
 
 }
