30f31e11a2778b956de8193ad2011cef24c385aa
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-11-15 15:48:20 +0100

    Create info.r

---
+++ system/info.r
@@ -0,0 +1,8 @@
+REBOL [
+	Title:   "Wift System Informations"
+	Author:  "Suriyaa Kudo"
+	File: 	 %info.r
+	Tabs:	 4
+	Rights:  "Copyright (C) 2015-present Suriyaa Kudo. All rights reserved."
+	License: "GNU - https://github.com/SuriyaaKudoIsc/wift/blob/trunk/LICENSE.md"
+]
